09a27367d5a50855f134820a9465ea61eabb0dd3
Manraj <manrajt@umich.edu>
2020-10-02 17:06:29 -0400

    Began comment section

--- DjangoBlog/blog/models.py
+++ DjangoBlog/blog/models.py
@@ -14,3 +14,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', args=(str(self.id)))
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.post.title + ' | ' + self.name
