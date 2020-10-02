2968d5a3e4c1e2b2d5e51889f37f45bff7ac6e8f
Manraj <manrajt@umich.edu>
2020-10-01 22:59:42 -0400

    Added detailed post view Removed cache files and sqlite3 files from repo

--- DjangoBlog/blog/views.py
+++ DjangoBlog/blog/views.py
@@ -6,3 +6,7 @@ from .models import Post
 class IndexView(ListView):
     model = Post
     template_name = "index.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_details.html"
