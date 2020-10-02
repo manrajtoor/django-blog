62618a6576985294e9bdb71741a10f189ebbb3b2
Manraj <manrajt@umich.edu>
2020-10-02 16:50:42 -0400

    Added Update post functionality Added Delete post functionality

--- DjangoBlog/blog/urls.py
+++ DjangoBlog/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView, PostDetailView, CreatePostView
+from .views import IndexView, PostDetailView, CreatePostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('post/<int:pk>', PostDetailView.as_view(), name="post"),
-    path('create_post/', CreatePostView.as_view(), name="create_post" )
+    path('create_post/', CreatePostView.as_view(), name="create_post" ),
+    path('post/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
+    path('post/delete/<int:pk>', DeletePostView.as_view(), name="delete_post")
 ]
--- DjangoBlog/blog/views.py
+++ DjangoBlog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 
 class IndexView(ListView):
@@ -11,8 +12,17 @@ class PostDetailView(DetailView):
     model = Post
     template_name = "post_details.html"
 
-
 class CreatePostView(CreateView):
     model = Post
     template_name = "create_post.html"
     fields = ('title', 'author', 'body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = "update_post.html"
+    fields = ['title', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = "delete_post.html"
+    success_url = reverse_lazy('index')
