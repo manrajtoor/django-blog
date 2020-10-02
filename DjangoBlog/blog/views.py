817caf49e85f01c068efe7f09d7523942370be60
Manraj <manrajt@umich.edu>
2020-10-01 21:43:44 -0400

    Added initial Post view, successfully fetching posts from db Updated gitignore with pycache files

--- DjangoBlog/blog/views.py
+++ DjangoBlog/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
 
-def index(request):
-    return render(request, 'index.html', {})
+class IndexView(ListView):
+    model = Post
+    template_name = "index.html"
